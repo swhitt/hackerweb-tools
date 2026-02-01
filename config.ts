3ee4cb44911d036a3319e5268615188c4048229e
Steve Whittaker <swhitt@gmail.com>
2026-02-01 00:21:42 -0600

    Release v0.0.1-8

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 // Version: bump `build` for each gist push, `version` for releases
 export const version = "0.0.1";
-export const build = 7;
+export const build = 8;
 export const fullVersion = `${version}-${build}`;
 
 /** Gist configuration for userscript auto-updates */
